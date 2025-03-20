520b28982154d8c2e07fa2af8307ccaed18ae425
Alhagie Bai Cham <cham207388@gmail.com>
2025-03-20 13:48:18 -0500

    deployed successfully and working

--- src/main/java/com/abc/Application.java
+++ src/main/java/com/abc/Application.java
@@ -1,5 +1,6 @@
 package com.abc;
 
+import com.abc.controller.CourseController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -10,7 +11,7 @@ import com.abc.controller.PingController;
 @SpringBootApplication
 // We use direct @Import instead of @ComponentScan to speed up cold starts
 // @ComponentScan(basePackages = "com.abc.controller")
-@Import({ PingController.class })
+@Import({ PingController.class, CourseController.class })
 public class Application {
 
     public static void main(String[] args) {
