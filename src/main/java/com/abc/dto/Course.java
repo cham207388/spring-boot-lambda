388824fb7a9822fae4d1b4bdf8b5da0e46d50c10
Alhagie Bai Cham <cham207388@gmail.com>
2025-03-20 08:11:42 -0500

    first commit. Deployed successfully

---
+++ src/main/java/com/abc/dto/Course.java
@@ -0,0 +1,14 @@
+package com.abc.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+
+    private int id;
+    private String name;
+    private double price;
+}
