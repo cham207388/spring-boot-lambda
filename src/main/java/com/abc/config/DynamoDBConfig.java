c59b16d528aabb068bea4a0fb95f24e6dd85b442
Alhagie Bai Cham <cham207388@gmail.com>
2025-03-22 19:36:10 -0500

    manual setters

--- src/main/java/com/abc/config/DynamoDBConfig.java
+++ src/main/java/com/abc/config/DynamoDBConfig.java
@@ -16,7 +16,7 @@ public class DynamoDBConfig {
     @Bean
     public DynamoDbClient dynamoDbClient() {
         return DynamoDbClient.builder()
-                .region(Region.of(System.getenv().getOrDefault("AWS_REGION", "us-east-1")))
+                .region(Region.of(System.getenv().getOrDefault("AWS_REGION", "us-east-2")))
                 .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
