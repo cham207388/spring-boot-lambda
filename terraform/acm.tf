76ed1fc232e14841cd169220fe654ce7a3c7dd1c
Alhagie Bai Cham <cham207388@gmail.com>
2025-03-23 08:29:40 -0500

    refactor

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -2,10 +2,3 @@ provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1"
 }
-
-# data "aws_acm_certificate" "dev_cert" {
-#   domain      = "dev.alhagiebaicham.com"
-#   statuses    = ["ISSUED"]
-#   most_recent = true
-#   provider    = aws.us-east-1
-# }
