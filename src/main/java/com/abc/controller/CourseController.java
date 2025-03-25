1d42db02060022609fc8249f3e7797cac6d87aef
cham207388 <cham207388@gmail.com>
2025-03-25 08:13:42 -0500

    logging (#6)
    * logging
    
    * jarkarta validation error
    
    * appName and env
    
    * no need for custom fields
    
    * WARN
    
    * custom fields
    
    * /api/v1
    
    * /api/v1

--- src/main/java/com/abc/controller/CourseController.java
+++ src/main/java/com/abc/controller/CourseController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/courses")
+@RequestMapping("/api/v1/courses")
 @RequiredArgsConstructor
 public class CourseController {
 
